e5bf3f79235388790f0903b74032e197332378cc
程序狗没有春天 <14320885+the-program-dog-has-no-spring@user.noreply.gitee.com>
2024-10-06 22:08:18 +0800

    整合了redis用于图形验证
    redis访问的是本机的地址所以本机要开启redis,已经配置好了一部分

--- xdclass-user-service/src/main/java/net/xdclass/controller/NotifyController.java
+++ xdclass-user-service/src/main/java/net/xdclass/controller/NotifyController.java
@@ -3,7 +3,9 @@ package net.xdclass.controller;
 import com.google.code.kaptcha.Producer;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
+import net.xdclass.util.CommonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author 程序狗没有春天
@@ -27,12 +30,19 @@ import java.io.IOException;
 public class NotifyController {
     @Autowired
     private Producer captchaProducer;
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
+    /**
+     *临时使用10分钟有效，方便测试
+     */
+    private static final long CAPTCHA_CODE_EXPIRED = 60 * 1000 * 10;
     @GetMapping("captcha")
     public void getCaptcha(HttpServletRequest request, HttpServletResponse response){
         String captchaText = captchaProducer.createText();
         log.info("图形验证码:{}", captchaText);
-
+        //存储
+        redisTemplate.opsForValue().set(getCaptchaKey(request), captchaText, CAPTCHA_CODE_EXPIRED, TimeUnit.MILLISECONDS);
         BufferedImage bufferedImage = captchaProducer.createImage(captchaText);
         ServletOutputStream outputStream = null;
         try{
@@ -43,5 +53,18 @@ public class NotifyController {
         } catch (IOException e) {
             log.error("获取图形化验证码异常:{}", e);
         }
+    }
+    /***
+     * 获取缓存的key
+     *
+     */
+    private String getCaptchaKey(HttpServletRequest request){
+        String ip = CommonUtil.getIpAddr(request);
+        String userAgent = request.getHeader("User-Agent");
+        String key = "user-service:capycha:" + CommonUtil.MD5(ip+userAgent);
+        log.info("ip={}", ip);
+        log.info("userAgent+{}", userAgent);
+        log.info("key:{}", key);
+        return key;
     }
 }
